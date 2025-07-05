03c391b8f15aa026c352b909c32d80c4420b5a30
Vital Travin <107115258+trwv55@users.noreply.github.com>
2025-07-05 14:24:51 +0300

    fix create task gradient svg

--- src/features/CreatTask/index.tsx
+++ src/features/CreatTask/index.tsx
@@ -2,19 +2,17 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import GradientSVG from "@/shared/assets/svg/app-top.svg";
-import gradient from "@/shared/assets/svg/app-top.svg";
+import { AppTopGradient } from "@/shared/assets/icons/AppTopGradient";
 import styles from "./createTask.module.css";
-import Image from "next/image";
 
 export const CreateTask = () => {
 	const router = useRouter();
 
 	return (
 		<div className="flex flex-col items-center justify-between h-full relative">
-			<div className={styles.gradient}>
-				<GradientSVG className={styles.gradientSvg} />
-			</div>
+                        <div className={styles.gradient}>
+                                <AppTopGradient className={styles.gradientSvg} />
+                        </div>
 			<h1>Create</h1>
 		</div>
 	);
