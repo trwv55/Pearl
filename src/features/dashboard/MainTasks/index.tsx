6de900d5bc3f8a1082329f842e91a19528817aa4
Vital Travin <107115258+trwv55@users.noreply.github.com>
2025-07-29 16:21:37 +0300

    Add MobX task cache with date range loading

--- src/features/dashboard/MainTasks/index.tsx
+++ src/features/dashboard/MainTasks/index.tsx
@@ -1,19 +1,24 @@
 import { ShowMainTasks } from "@/widgets/mainPage/ShowMainTasks";
 import styles from "../shared/styles/TasksTop.module.css";
 import { MainTasksCount } from "@/widgets/mainPage/shared/tasksCount/MainTasksCount";
+import { observer } from "mobx-react-lite";
+import { taskStore } from "@/entities/task/store";
+import { dateStore } from "@/entities/date/store";
 
 // interface DisplayMainTasksProps {
 
 // }
 
-export const MainTasks: React.FC = () => {
-	return (
-		<div className={styles.wrap}>
-			<div className={styles.top}>
-				<h2>Главные задачи</h2>
-				<MainTasksCount current={0} max={3} />
-			</div>
-			<ShowMainTasks tasks={[]} showDots />
-		</div>
-	);
-};
+export const MainTasks = observer(() => {
+        const { mainTasks } = taskStore.getTasksGroupedByMain(dateStore.selectedDate);
+
+        return (
+                <div className={styles.wrap}>
+                        <div className={styles.top}>
+                                <h2>Главные задачи</h2>
+                                <MainTasksCount current={mainTasks.length} max={3} />
+                        </div>
+                        <ShowMainTasks tasks={mainTasks} showDots />
+                </div>
+        );
+});
